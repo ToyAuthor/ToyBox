b98ff102d4c69ac7329325eeb12bdf83688173bf
ToyAuthor <eye5002003@gmail.com>
2016-06-03 21:41:27 +0800

    Add more feature to UTF converter.

--- include/toy/utf/Convert.hpp
+++ include/toy/utf/Convert.hpp
@@ -1,16 +1,24 @@
 
 #pragma once
 
-#include "toy/Standard.hpp"
-
+#include <cstdint>
+#include <string>
+#include "toy/CompilerConfig.hpp"
 
 namespace toy{
 namespace utf{
 
 TOY_API std::basic_string<wchar_t> UTF8ToWChar(std::string str);
 TOY_API std::string WCharToUTF8(std::basic_string<wchar_t> str);
+
 TOY_API std::basic_string<uint16_t> UTF8ToUTF16(std::string str);
 TOY_API std::basic_string<uint32_t> UTF8ToUTF32(std::string str);
+TOY_API std::basic_string<uint32_t> UTF16ToUTF32(std::basic_string<uint16_t> str);
+TOY_API std::basic_string<uint16_t> UTF32ToUTF16(std::basic_string<uint32_t> str);
+TOY_API std::string UTF16ToUTF8(std::basic_string<uint16_t> str);
 TOY_API std::string UTF32ToUTF8(std::basic_string<uint32_t> str);
+
+TOY_API void UTF8ToWChar(const char    *input, wchar_t  *output, int length);
+TOY_API void WCharToUTF8(const wchar_t *input, char     *output, int length);
 
 }}
--- source/toy/utf/Convert.cpp
+++ source/toy/utf/Convert.cpp
@@ -83,6 +83,22 @@ std::basic_string<uint16_t> UTF8ToUTF16(std::string str)
 	return result;
 }
 
+std::string UTF16ToUTF8(std::basic_string<uint16_t> str)
+{
+	std::string    result(str.size()*4,'\0');
+
+	ConversionFlags    flag   = strictConversion;
+	const uint16_t*    data16 = str.data();
+	uint8_t*           data8  = const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(result.data()));
+	ConversionResult   error  = ConvertUTF16toUTF8( &data16, data16+str.size(), &data8, data8+result.size(), flag );
+
+	(void)error;
+
+	StripTrailing(&result);
+
+	return result;
+}
+
 std::basic_string<uint32_t> UTF8ToUTF32(std::string str)
 {
 	std::basic_string<uint32_t>   result(str.size(),'\0');
@@ -106,8 +122,7 @@ std::string UTF32ToUTF8(std::basic_string<uint32_t> str)
 	ConversionFlags    flag   = strictConversion;
 	const uint32_t*    data32 = str.data();
 	uint8_t*           data8  = const_cast<uint8_t*>(reinterpret_cast<const uint8_t*>(result.data()));
-
-	ConversionResult  error = ConvertUTF32toUTF8( &data32, data32+str.size(), &data8, (data8+result.size()),flag);
+	ConversionResult   error  = ConvertUTF32toUTF8( &data32, data32+str.size(), &data8, data8+result.size(), flag );
 
 	(void)error;
 
@@ -116,6 +131,66 @@ std::string UTF32ToUTF8(std::basic_string<uint32_t> str)
 	return result;
 }
 
+std::basic_string<uint32_t> UTF16ToUTF32(std::basic_string<uint16_t> str)
+{
+	std::basic_string<uint32_t>    result(str.size(),'\0');
 
-}//namespace utf{
-}//namespace toy{
+	ConversionFlags    flag   = strictConversion;
+	const uint16_t*    data16 = str.data();
+	uint32_t*          data32 = const_cast<uint32_t*>(result.data());
+	ConversionResult   error  = ConvertUTF16toUTF32( &data16, data16+str.size(), &data32, data32+result.size(), flag );
+
+	(void)error;
+
+	StripTrailing(&result);
+
+	return result;
+}
+
+std::basic_string<uint16_t> UTF32ToUTF16(std::basic_string<uint32_t> str)
+{
+	std::basic_string<uint16_t>    result(str.size()*2,'\0');
+
+	ConversionFlags    flag   = strictConversion;
+	const uint32_t*    data32 = str.data();
+	uint16_t*          data16 = const_cast<uint16_t*>(result.data());
+	ConversionResult   error  = ConvertUTF32toUTF16( &data32, data32+str.size(), &data16, data16+result.size(), flag );
+
+	(void)error;
+
+	StripTrailing(&result);
+
+	return result;
+}
+
+void UTF8ToWChar( const char *input, wchar_t *output, int length )
+{
+	ConversionFlags  flag  = strictConversion;
+	const uint8_t*   data8 = reinterpret_cast<const uint8_t*>(input);
+
+	#if defined(TOY_WINDOWS)
+		uint16_t*   data16 = reinterpret_cast<uint16_t*>(output);
+		ConversionResult   error = ConvertUTF8toUTF16( &data8, data8+length, &data16, data16+length, flag );
+	#else
+		uint32_t*   data32 = reinterpret_cast<uint32_t*>(output);
+		ConversionResult   error = ConvertUTF8toUTF32( &data8, data8+length, &data32, data32+length, flag );
+	#endif
+	(void)error;
+}
+
+void WCharToUTF8( const wchar_t *input, char *output, int length )
+{
+	ConversionFlags  flag  = strictConversion;
+	uint8_t*         data8 = reinterpret_cast<uint8_t*>(output);
+
+	#if defined(TOY_WINDOWS)
+		const uint16_t*   data16 = reinterpret_cast<const uint16_t*>(input);
+		ConversionResult   error = ConvertUTF16toUTF8( &data16, data16+length, &data8, data8+length*4, flag );
+	#else
+		const uint32_t*   data32 = reinterpret_cast<const uint32_t*>(input);
+		ConversionResult   error = ConvertUTF32toUTF8( &data32, data32+length, &data8, data8+length*4, flag );
+	#endif
+	(void)error;
+}
+
+}}
