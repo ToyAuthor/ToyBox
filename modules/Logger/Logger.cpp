8875bea50edae875edda05a6496eeb6a2bc263b7
ToyAuthor <eye5002003@gmail.com>
2017-07-01 22:02:06 +0800

    Add a lua module for printing message.

---
+++ modules/Logger/Logger.cpp
@@ -0,0 +1,54 @@
+
+#include <luapp.hpp>
+#include <toy/Log.hpp>
+#include <toy/Utf.hpp>
+
+static int ToyBoxIsUTF8(lua::NativeState L)
+{
+	lua::Str   str;
+	lua::CheckVarFromLua(L,&str,-1);
+	lua::Pop(L,1);
+	lua::Bool  result = toy::utf::IsUtf8(str);
+	lua::PushVarToLua( L, result );
+	return 1;
+}
+
+inline static void PrintString(lua::NativeState L)
+{
+	lua::Str   str;
+
+	lua::CheckVarFromLua(L,&str,-1);
+	lua::Pop(L,1);
+
+	toy::Logger<<str;
+}
+
+static int ToyBoxLogger(lua::NativeState L)
+{
+	PrintString(L);
+	return 1;
+}
+
+static int ToyBoxLoggerWithNewLine(lua::NativeState L)
+{
+	PrintString(L);
+	toy::Logger<<toy::NextLine;
+	return 1;
+}
+
+#if defined(_WIN32)
+	#define MY_DLL_API __declspec(dllexport)
+#else
+	#define MY_DLL_API
+#endif
+
+extern "C" MY_DLL_API int luaopen_toy_logger(lua::NativeState L)
+{
+	lua::State<>    lua(L);
+
+	lua.setFunc( "printf", ToyBoxLogger );
+	lua.setFunc( "print",  ToyBoxLoggerWithNewLine );
+	lua.setFunc( "isUTF8", ToyBoxIsUTF8 );
+
+	return 1;
+}
--- resource/lua/examples/HelloWorld/main.lua
+++ resource/lua/examples/HelloWorld/main.lua
@@ -1,2 +1,3 @@
 
-print("Hello World!")
+local  msg = require "toy.logger"
+msg.print("ToyBox;玩具箱;おもちゃ箱;장난감 상자;खिलौनो का बक्सा")
