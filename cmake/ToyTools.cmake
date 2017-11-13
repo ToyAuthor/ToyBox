8305476a0dcf777bc6614dfad2d1e5afae9c0344
ToyAuthor <eye5002003@gmail.com>
2017-11-13 21:52:12 +0800

    Organize *.cmake files.

--- cmake/ToyTools.cmake
+++ cmake/ToyTools.cmake
@@ -6,6 +6,13 @@ include("${TOY_ROOT_SOURCE_DIR}/cmake/ToyCheckEnvironment.cmake" REQUIRED)
 
 macro(toy_IncludeTools)
 
+	if(NOT CMAKE_BUILD_TYPE)
+	#	set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build" FORCE)
+		set(CMAKE_BUILD_TYPE "Debug"   CACHE STRING "Choose the type of build" FORCE)
+	endif()
+
+	option( TOY_ENABLE_SHARED_LIBS   "Make shared library"                  ON )
+
 	set(TOY_RESOURCE_PATH       ${TOY_ROOT_SOURCE_DIR}/resource)
 	set(TOY_DEFAULT_INCLUDE_DIR ${TOY_ROOT_SOURCE_DIR}/include ${TOY_ROOT_SOURCE_DIR}/source)
 	set(TOY_DEFAULT_LINK_DIR    ${TOY_ROOT_BINARY_DIR}/lib)
