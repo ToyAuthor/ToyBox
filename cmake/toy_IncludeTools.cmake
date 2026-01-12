0b1eb0837a5b4e77fae44f11071f7031fe37cc5e
ToyAuthor <eye5002003@gmail.com>
2026-01-12 23:14:57 +0800

    source->src

--- cmake/toy_IncludeTools.cmake
+++ cmake/toy_IncludeTools.cmake
@@ -12,7 +12,7 @@ macro(toy_IncludeTools)
 	include("${TOY_ROOT_SOURCE_DIR}/cmake/toy_CheckEnvironment.cmake" REQUIRED)
 
 	set(TOY_RESOURCE_PATH       ${TOY_ROOT_SOURCE_DIR}/resource)
-	set(TOY_DEFAULT_INCLUDE_DIR ${TOY_ROOT_SOURCE_DIR}/include ${TOY_ROOT_SOURCE_DIR}/source)
+	set(TOY_DEFAULT_INCLUDE_DIR ${TOY_ROOT_SOURCE_DIR}/include ${TOY_ROOT_SOURCE_DIR}/src)
 	set(TOY_DEFAULT_LINK_DIR    ${TOY_ROOT_BINARY_DIR}/lib)
 
 	toy_Option()
